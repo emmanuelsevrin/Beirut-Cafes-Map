6c61363a2a5d35f4ca124033ca3c994bd549dc2c
Emmanuel Sevrin <emmanuel.sevrin@gmail.com>
2018-04-02 13:53:30 +0300

    Updated ViewModel

--- app.js
+++ app.js
@@ -74,58 +74,60 @@ var Model = {
 	centerofMap: {lat: 33.888630, lng: 35.495480},
 }
 
-var Place = function(place_data){
-	this.name = ko.observable(place_data.name);
-	this.lat = place_data.lat;
-	this.lng = place_data.lng;
-	this.description = place_data.description;
-	this.external_link = place_data.external_link;
-	this.image = place_data.image;
-	this.zomato_id = place_data.zomato_id
-}
-
-
 
 var ViewModel = {
 
+	//initializes the app
 	init: function(){
 		//PlacesList is the list of places to display in the view
-		PlacesList = ko.observableArray([]);
-
+		//fetches all the locations from the model
 		this.getallPlaces();
 
+		//creates a new map and add all the locations
 		View.newMap();
 		View.addLocations();
+
+		//updates all infowindow with the Zomato Rating 
+		ViewModel.updateallinfowindow();
 	},
 
-	//pushes all places from the Model to PlaceList
+	//constructor for places from the Model
+	Place: function(place_data){
+		this.name = ko.observable(place_data.name);
+		this.lat = place_data.lat;
+		this.lng = place_data.lng;
+		this.description = place_data.description;
+		this.external_link = place_data.external_link;
+		this.image = place_data.image;
+		this.zomato_id = place_data.zomato_id
+		this.visible = ko.observable(true);
+	},
+
+	//fetches all the locations from the model
 	getallPlaces: function(){
 		
-		PlacesList([]);
-
 		Model.keyPlaces.forEach(function(place_data){
-			self.PlacesList.push( new Place(place_data))
+			View.placesList.push(new ViewModel.Place(place_data));
 		});
 	},
 
 	//sorts PlacesList according to the keyword entered by the user
 	sortPlaces: function(entry){
 		
-		this.getallPlaces();
-
+		//the value entered by the user in the filter field
 		var filterString = $("#filter").val();
 
-		for(k=0; k<PlacesList().length; k++) {
-			if(!PlacesList()[k].name().includes(filterString)){
-				PlacesList.splice(k, 1);
-				k=k-1; // we need to reduce k by one because we've just removed one element from the table where we iterate
+		//going through each item and removing them if they don't match the string
+		for(k=0; k<View.placesList.length; k++) {
+			if(!View.placesList[k].name().includes(filterString)){
+				View.placesList[k].visible(false);
 			}
 		}
-
 	//update the markers according to the latest list of Places to display
 	View.addLocations();	
 	},
 
+	//as the name indicates, update a specific infowindow with the zomato review
 	updateInfowindowWithZomatoReview: function(infowindow, index){
 		var myHeaders = new Headers({
 			"user-key": Zomato_user_key
@@ -138,25 +140,34 @@ var ViewModel = {
   			  return response.json();
  		 })
   		.then(function(myJson) {
-  		  //console.log(myJson); // in case we should look at the JSON
-  		  console.log(myJson.user_rating.aggregate_rating);
-  		  console.log(local_zomato_id);
-  		  infowindow.open(View.map);      	
+
+  		//if closed, opens up the infowindow then uses jQuery to update it
+  		var infowindowwasopen = true;
+  		  if(infowindow.getMap() == null){
+  		  	 infowindowwasopen = false;
+  		  	 infowindow.open(View.map);      		
+  		  }
   		  $(`.rating.${local_zomato_id}`).html(myJson.user_rating.aggregate_rating);
-  		  infowindow.close(View.map);
-  		  //View.updateinfowindow_withRating(Place_ID, myJson.user_rating.aggregate_rating);
-  		  //View.infoWindowList[Place_ID] // ViewModel.ZomatoReview(myJson.user_rating.aggregate_rating);
+  		  
+  		  if(infowindowwasopen == false){
+  		  		infowindow.close(View.map);
+  			}
  		 });
 
-	}
+	},
 
+	updateallinfowindow: function(){
+		View.infoWindowList.forEach(function(infowindow, index){
+			ViewModel.updateInfowindowWithZomatoReview(infowindow, index);
+		});
+	}
 
 }
 
 var View = {
-
 	//those three variables represent: 1) the map that will be displayed, 2) the list of markers, 3) the list of infowindows 
 	map,
+	placesList: ko.observableArray(),
 	markersList:[],
 	infoWindowList:[],
 
@@ -168,22 +179,70 @@ var View = {
 		});
 	},
 
+	//create new markers and add them tothe Markerlist 
 	addLocations: function(){
 		
+		//first empties the marker list to reset
 		View.markersList.forEach(function(marker){
 			marker.setMap(null);
 		});
 
 		View. markersList = [];
 
-		PlacesList().forEach(function(value){
-			View.markersList.push(new mapMarker(value));
+		View.placesList().forEach(function(value){
+			View.markersList.push(new View.mapMarker(value));
 		});
 	},
 
+	//update map to only keep the markers and infowindows for items open
+	updateMap: function(){
+		View.placesList().forEach(place, index){
+			if(!place.visible){
+				markersList[index].
+				infoWindowList[index].close();
+			}
+			else{
+
+			}
+		}
+	},
+
+	//constructor for a new map marker
+	mapMarker: function(value){
+
+		var mylatlong = {lat: value.lat, lng: value.lng}
+
+		var marker = new google.maps.Marker({
+					position: mylatlong,
+					map:View.map,
+					title: value.name()
+				});
+
+		var infoWindow = new google.maps.InfoWindow({
+	        content: View.Createdescription(value.name(), value.description, value.external_link, value.image, value.zomato_id),
+	        maxWidth: 400
+	    });
+
+	    View.infoWindowList.push(infoWindow);
+
+		marker.addListener('click', function() {
+
+	          marker.setAnimation(google.maps.Animation.BOUNCE);
+	          setTimeout(function(){marker.setAnimation(null); }, 1400);
+
+	          //go through each infowindow and close it before opening a new one 
+	          View.infoWindowList.forEach(function(infowindow_inlist){
+	          	infowindow_inlist.close();
+	          })
+
+	 		  infoWindow.open(View.map, marker);      	
+	        });
 
 
-	//creates a text description that can be inserted in the marker
+		return marker
+	},
+
+	//creates a text description that can be inserted in the infowwindow
 	Createdescription: function(title, content, external_link, image, zomato_id){
 		html_string ='<div class="row">' +
 		'<div flex=30% class="column"><img width="100" height="100" src="images/' + image + '.jpg">'+ '</div>' +
@@ -193,133 +252,17 @@ var View = {
 		content + 
 		'</div>' + 
 		'<p> <a href="'+ external_link + '">'+ external_link + '</a></p>' + 
- 		`<p> Zomato Rating: <span class="rating ${zomato_id}"></span></p>` +  
+ 		`<p> Zomato Rating: <span class="rating ${zomato_id}">N/A</span></p>` + //the N/A will be updated if/ once we have loaded the zomato review 
  		'</div>' + 
  		'</div>'
  		
- 		//ViewModel.getZomatoReview(title, zomato_id);
 		return html_string
 	},
-
-	updateallinfowindow: function(){
-		console.log('running all infowindow')
-		View.infoWindowList.forEach(function(infowindow, index){
-			console.log('running for ' + infowindow + index);
-			ViewModel.updateInfowindowWithZomatoReview(infowindow, index);
-		});
-	}
-
-	// updateinfowindow_withRating: function(place_ID, rating){
-	// 	View.infoWindowList[place_ID].
-	// }
 
 }
 
 
-var mapMarker = function(value){
-
-	var mylatlong = {lat: value.lat, lng: value.lng}
-
-	var marker = new google.maps.Marker({
-				position: mylatlong,
-				map:View.map,
-				title: value.name()
-			});
-
-	var infoWindow = new google.maps.InfoWindow({
-        content: View.Createdescription(value.name(), value.description, value.external_link, value.image, value.zomato_id),
-        maxWidth: 400
-    });
-
-    View.infoWindowList.push(infoWindow);
-
-	marker.addListener('click', function() {
-
-          marker.setAnimation(google.maps.Animation.BOUNCE);
-          setTimeout(function(){marker.setAnimation(null); }, 1400);
-
-          //go through each infowindow and close it before opening a new one 
-          View.infoWindowList.forEach(function(infowindow_inlist){
-          	infowindow_inlist.close();
-          })
-
- 		  infoWindow.open(View.map, marker);      	
-        });
-
-
-	return marker
-	}
 
 ViewModel.init();
 
-View.updateallinfowindow();
-
-//mapMarker.prototype = Object.create(google.maps.Marker.prototype);
-
-//var essai = new mapMarker({lat, });
-
-//essai.setMap(View.map);
-
 ko.applyBindings(ViewModel);
-
-//
-
-// var data = null;
-
-// var xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
-
-// xhr.addEventListener("readystatechange", function () {
-//   if (this.readyState === 4) {
-//     console.log(this.responseText);
-//   }
-// });
-
-// xhr.open("GET", "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=209094a629c7432e9e3c9e236c26a674");
-// xhr.setRequestHeader("Cache-Control", "no-cache");
-// xhr.setRequestHeader("Postman-Token", "7da2b02b-67a5-46db-a3d8-8ddc7953f106");
-
-// xhr.send(data);
-// xhr.send(data);
-
-
-
-
-// var myHeaders = new Headers({
-// Authorization: OAuth oauth_consumer_key="Yfb1tIAV7nXjXUkSJ01DXwbok",
-// oauth_token="247827096-EmlltmQsI0R2D5F9BM68gKWZkJRUZcqfVfsNUs0K",
-// oauth_signature_method="HMAC-SHA1",
-// oauth_timestamp="1522576131",
-// oauth_nonce="sM6tLUeUmFX",
-// oauth_version="1.0",
-// oauth_signature="ZtSv1jANmBwx6QZ9iM0lpzE0agQ%3D"
-// });
-
-// var ab = fetch('https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=popular', {headers: myHeaders})
-// .then(response => console.log(response))
-
-
-// function locatePlaces(){
-
-// 	var geocoder = new google.maps.Geocoder();
-
-// 	//keyPlaces.forEach(function(){
-
-// 		geocoder.geocode(
-// 			{	address: 'Aaliya Books, Beirut, Lebanon'
-// 		}, function(results, status){
-// 			console.log(results);
-// 			//if (status == google.maps.Geocoderstatus.OK){
-
-// 			//}
-// 			}
-
-// 		);
-// 	//});
-//}
-
-
-// Constructor creates a new View.map - only center and zoom are required.
-
-
-
